ac4e4396566951a4ab7bfe01cbbac626b30a608b
Wouter Wijsman <wwijsman@live.nl>
2026-06-28 18:11:42 +0200

    Add libge

--- gcc/config/mips/psp.h
+++ gcc/config/mips/psp.h
@@ -30,7 +30,7 @@ Boston, MA 02111-1307, USA.  */
     %{pg:-lpspprof} \
     --end-group \
     -lpsputility -lpsprtc -lpspnet_inet -lpspnet_resolver \
-    -lpspsdk -lpspmodinfo -lpspuser -lpspgu"
+    -lpspsdk -lpspmodinfo -lpspuser -lpspge -lpspgu"
 
 /* Override the startfile spec to include crt0.o. */
 #undef STARTFILE_SPEC
